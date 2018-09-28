01652ccec30f5cabb2b3ee4cd8360bf76643bcc1
Jordan Paz <jordandavidpaz@gmail.com>
2018-09-28 00:30:20 -0700

    finished the player bar checkpoint

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
+
 
 
 class Album extends Component {
@@ -71,6 +73,14 @@ class Album extends Component {
      }
    }
 
+   handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+    }
+
   render () {
     return(
       <section className="album">
@@ -102,6 +112,12 @@ class Album extends Component {
              )}
           </tbody>
         </table>
+        <PlayerBar
+           isPlaying={this.state.isPlaying}
+           currentSong={this.state.currentSong}
+           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}
+         />
       </section>
     );
   }
