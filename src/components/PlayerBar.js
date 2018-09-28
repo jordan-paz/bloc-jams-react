b32be87fdf920354318d7338ea0306de35b49d71
Jordan Paz <jordandavidpaz@gmail.com>
2018-09-28 00:41:24 -0700

    finished the player bar assignment

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -5,19 +5,19 @@ import React, { Component } from 'react';
      return (
        <section className="player-bar">
         <section id="buttons">
-           <button id="previous" onClick={this.props.handlePrevClick}>
+          <button id="previous" onClick={this.props.handlePrevClick}>
             <ion-icon name="skip-backward"></ion-icon>
-            </button>
-            <button id="play-pause" onClick={this.props.handleSongClick} >
+          </button>
+          <button id="play-pause" onClick={this.props.handleSongClick} >
             <span>{this.props.isPlaying ? <ion-icon name="pause"></ion-icon> : <ion-icon name="play"></ion-icon>}</span>
-            </button>
-            <button id="next">
-            <span className="ion-skip-forward"></span>
+          </button>
+          <button id="next" onClick={this.props.handleNextClick}>
+            <ion-icon name="skip-forward"></ion-icon>
           </button>
         </section>
         <section id="time-control">
           <div className="current-time">–:––</div>
-          <input type="range" className="seek-bar" value="0" />
+            <input type="range" className="seek-bar" value="0" />
           <div className="total-time">–:––</div>
         </section>
         <section id="volume-control">
